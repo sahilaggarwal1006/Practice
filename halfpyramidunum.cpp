0cd3be06e1eeabe212798be0b04dbecc1f195f4d
sahil aggarwal <sahilaggarwal1006@gmail.com>
2022-01-19 23:47:23 +0530

    useless

--- halfpyramidunum.cpp
+++ halfpyramidunum.cpp
@@ -4,13 +4,15 @@ using namespace std;
 int main()
 {
     int n;
+    int count=1;
     cout << "Enter n";
     cin >> n;
-    for (int i = 1; i <= n; i++)
+    for (int i = 2; i <= n; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = 2; j <= i; j++)
         {
-            cout << i;
+            cout << count;
+            count++;
         }
         
         cout << endl;
