ab17f4b7b7ca23c5b96cd34360d417e1108fcfb8
sahil aggarwal <sahilaggarwal1006@gmail.com>
2022-01-22 22:27:43 +0530

    new and interesting patterns

---
+++ Invertedpattern.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter n";
+    cin >> n;
+    for (int i = n; i >= 1; i--)
+    {
+       for(int j = 1;j <= i;j++){
+           cout << j ;
+       }
+       cout << endl;
+    }
+    getch();
+    return 0;
+}
